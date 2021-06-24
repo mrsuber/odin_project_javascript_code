f93912c4a40668b58c879905268d9aefbf809354
mohamad <msiysinyuy@gmail.com>
2021-06-24 14:18:56 +0100

    the first javascript file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # odin_project_javascript_code
+***this is to keep track of different java script code i write as a result of odin***
+
+**javaScript01 is to educate about class syntax**
+1. class MyClass {
+  // class methods
+  constructor() { ... }
+  method1() { ... }
+  method2() { ... }
+  method3() { ... }
+  ...
+}
+Then use new MyClass() to create a new object with all the listed methods.
+
+The constructor() method is called automatically by new, so we can initialize the object there.
+
+For example:
---
+++ javaScript01.js
@@ -0,0 +1,26 @@
+learing about class syntax
+
+
+class User {
+
+  constructor(first_name,last_name,age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.age= age;
+  }
+
+  wellcomeNewUser() {
+    if(this.age<=0){
+      return"not real age";
+    }
+    else if(this.age>0 && this.age<=20){
+      return ("welcome "+this.first_name+" "+this.last_name+", you are young");
+    }
+    else return  ("welcome "+this.first_name+" "+this.last_name+", you are old");
+  }
+
+}
+
+// Usage:
+let user = new User("John","Smith", 0);
+user.wellcomeNewUser();
